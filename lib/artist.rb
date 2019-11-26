dd307e3d4c03485612e5f310dbfc11b5c74bd870
Logan Smith <logsmith06@gmail.com>
2019-11-26 21:57:30 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,9 +1,15 @@
 require 'pry'
 class Artist
-  attr_accessor :name
+  attr_accessor :name, :song
   @@songs = []
   def initialize(name)
     @name = name
   end
+
+  def songs(song)
+    @song = song 
+  end 
+    
+    
 
 end
